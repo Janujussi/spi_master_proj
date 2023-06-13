578374bf604b0710bc48edfb4c97c71d654da6d9
“Janujussi” <jancameron.morrison@gmail.com>
2023-06-13 01:33:07 -0400

    continued on gpio driver

--- Inc/gpio_driver.h
+++ Inc/gpio_driver.h
@@ -1,11 +1,66 @@
+/**
+ * Header of gpio driver
+ * 
+ * Author: Jan Morrison
+*/
+
 #ifndef GPIO_DRIVER_H
 #define GPIO_DRIVER_H
 
 #include <stdint.h>
 #include "stm32l476rg.h"
 
+#define SET                     1
+#define RESET                   0
+
+#define INPUT_MODE              0
+#define OUTPUT_MODE             1
+#define AF_MODE                 2
+#define ANALOG_MODE             3
+
+#define OUTPUT_PUSH_PULL        0
+#define OUTPUT_OPEN_DRAIN       1
+
+#define LOW_SPEED               0
+#define MEDIUM_SPEED            1
+#define HIGH_SPEED              2
+#define VERY_HIGH_SPEED         3
+
+#define NO_PUPD                 0
+#define PULL_UP                 1
+#define PULL_DOWN               2
+
+#define AF0                     0b0000
+#define AF1                     0b0001
+#define AF2                     0b0010
+#define AF3                     0b0011
+#define AF4                     0b0100
+#define AF5                     0b0101
+#define AF6                     0b0110
+#define AF7                     0b0111
+#define AF8                     0b1000
+#define AF9                     0b1001
+#define AF10                    0b1010
+#define AF11                    0b1011
+#define AF12                    0b1100
+#define AF13                    0b1101
+#define AF14                    0b1110
+#define AF15                    0b1111
+
+typedef struct {
+    uint8_t PIN;
+    uint8_t PORT_MODE;
+    uint8_t OUTPUT_TYPE;
+    uint8_t OUTPUT_SPEED;
+    uint8_t PUPD;
+    uint8_t INPUT_DATA;
+    uint8_t OUTPUT_DATA;
+    uint8_t ALTERNATE_FUNCTION;
+} GPIO_Config_t;
+
 typedef struct {
     GPIO_Reg_t* pGPIOx;  // GPIO port (address)
+    GPIO_Config_t GPIOConfig; // GPIO config
 } GPIO_Handle_t;
 
 /**
@@ -16,7 +71,7 @@ typedef struct {
  * 
  * @return None
 */
-void GPIO_Init(GPIO_Handle_t GPIO_Handle, uint32_t GPIO_Port);
+void GPIO_Init(GPIO_Handle_t GPIO_Handle);
 
 /**
  * Uninitialize GPIO
---
+++ Src/gpio_driver.c
@@ -0,0 +1,51 @@
+/**
+ * Implementation of gpio_driver.h
+ * 
+ * Author: Jan Morrison
+*/
+
+#include "gpio_driver.h"
+
+void GPIO_Init(GPIO_Handle_t* GPIO_Handle) {
+    GPIO_Reg_t* pGPIOx = GPIO_Handle->pGPIOx;
+    GPIO_Config_t GPIOConfig = GPIO_Handle->GPIOConfig;
+    uint8_t GPIOPin = GPIO_Handle->GPIOConfig.PIN;
+
+//---------------work on enable clock
+    // Enable Clock
+    switch((int)pGPIOx) {
+        case((int)GPIOA):
+            break;
+        case((int)GPIOB):
+            break;
+        case((int)GPIOC):
+            break;
+        case((int)GPIOD):
+            break;
+        case((int)GPIOE):
+            break;
+        case((int)GPIOF):
+            break;
+        case((int)GPIOG):
+            break;
+        case((int)GPIOH):
+            break;
+    }
+
+    // Port Mode
+    pGPIOx->MODER |= (GPIOConfig.PORT_MODE << 2 * GPIOPin);
+
+    if (GPIOConfig.PORT_MODE == OUTPUT_MODE) {
+        // Output Type
+        pGPIOx->OTYPER |= (GPIOConfig.OUTPUT_TYPE << GPIOPin);
+
+        // Output Speed
+        pGPIOx->OSPEEDR |= (GPIOConfig.OUTPUT_SPEED << 2 * GPIOPin);
+    }
+
+    // PUPDR
+    pGPIOx->PUPDR |= (GPIOConfig.PUPD << 2 * GPIOPin);
+
+    // Alternate Function
+    pGPIOx->AFR[GPIOPin / 8] |= (GPIOConfig.ALTERNATE_FUNCTION << 4 * GPIOPin)
+}
